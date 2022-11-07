405cdebe23fc9f80e11b67964c6a066ed04dbc4d
David Eduardo Velandia Laverde <111475825+79155637@users.noreply.github.com>
2022-11-06 22:25:49 -0500

    Delete Loopback/BackendPedidos/dist directory

--- Loopback/BackendPedidos/dist/repositories/producto.repository.d.ts
+++
@@ -1,6 +0,0 @@
-import { DefaultCrudRepository } from '@loopback/repository';
-import { MongodbDataSource } from '../datasources';
-import { Producto, ProductoRelations } from '../models';
-export declare class ProductoRepository extends DefaultCrudRepository<Producto, typeof Producto.prototype.id, ProductoRelations> {
-    constructor(dataSource: MongodbDataSource);
-}
